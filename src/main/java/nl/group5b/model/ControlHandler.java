b2109fd1963d38498e7f4662c128e015a0b1973b
hiddevanesch <hiddevanesch@hotmail.com>
2023-03-11 21:07:38 +0100

    Added DriveHandler (for future usage in Breitenberg vehicles)

--- src/main/java/nl/group5b/model/ControlHandler.java
+++ src/main/java/nl/group5b/model/ControlHandler.java
@@ -3,6 +3,6 @@ package nl.group5b.model;
 import nl.group5b.engine.Renderer;
 
 public interface ControlHandler {
-
+    // This function should "move" a body based on user input
     public void move(long window, Renderer renderer);
 }
---
+++ src/main/java/nl/group5b/model/DriveHandler.java
@@ -0,0 +1,8 @@
+package nl.group5b.model;
+
+import nl.group5b.engine.Renderer;
+
+public interface DriveHandler {
+    // This function should "move" a body based on body behaviour
+    public void move(Renderer renderer);
+}
